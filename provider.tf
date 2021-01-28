780a2467ef04401a33ac6aaa08a3f888fca8031e
msundeep4 <60873838+msundeep4@users.noreply.github.com>
2021-01-28 17:39:11 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "ibmcloud" {
-  ibmid              = "${var.ibmid}"
-  ibmid_password     = "${var.ibmidpw}"
-  softlayer_timeout  = 300
-  softlayer_account_number = "${var.slaccountnum}"
-}
